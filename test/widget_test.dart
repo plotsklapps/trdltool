44e2be9f9188b7ed606267e3d2af862b68617194
plotsklapps <plotsklapps@gmail.com>
2025-09-12 19:56:56 +0200

    - Added appIcon.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:trdltool/main.dart';
 
 void main() {
@@ -19,7 +18,7 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Tap the '+' icons and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
